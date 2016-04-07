419aedfd8d2a888b6c517b2bddf2f43ff803ef86
Teeh Amaral <contato@teehamaral.me>
2016-04-07 18:40:44 -0300

    Add new URLs

--- temba/orgs/urls.py
+++ temba/orgs/urls.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.conf.urls import url
-from .views import OrgCRUDL, UserSettingsCRUDL, TopUpCRUDL, UserCRUDL, check_login, StripeHandler
+from .views import OrgCRUDL, UserSettingsCRUDL, TopUpCRUDL, UserCRUDL, check_login, StripeHandler, OrderPaymentCRUDL
 
 urlpatterns = OrgCRUDL().as_urlpatterns()
 urlpatterns += UserSettingsCRUDL().as_urlpatterns()
 urlpatterns += TopUpCRUDL().as_urlpatterns()
+urlpatterns += OrderPaymentCRUDL().as_urlpatterns()
 urlpatterns += UserCRUDL().as_urlpatterns()
 
 urlpatterns += [
