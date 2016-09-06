aa2a3ec503aa133c83a72aa042c879160233b08c
Teeh Amaral <contato@teehamaral.me>
2016-09-05 21:35:26 -0300

    Add migrations merge

---
+++ temba/contacts/migrations/0044_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contacts', '0043_merge'),
+        ('contacts', '0042_auto_20160830_1727'),
+    ]
+
+    operations = [
+    ]
