3fa0cb1ee9fc55d57473684c46290ce79bfcc45d
Teeh Amaral <contato@teehamaral.me>
2017-02-15 20:31:31 -0300

    Remove migrate command from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,12 @@ set -e
 
 case $1 in
  runserver)
-  cd /udo-rapidpro; python manage.py migrate;python manage.py runserver 0.0.0.0:8000
+  cd /udo-rapidpro; python manage.py runserver 0.0.0.0:8000
   ;;
  uwsgi_server)
-  python manage.py migrate
   uwsgi  --socket app.sock --wsgi-file temba/wsgi.py --chmod-socket=666
   ;;
  supervisor)
-  python manage.py migrate
   /usr/bin/supervisord -n -c /udo-rapidpro/supervisor-app.conf
   ;;
 esac
