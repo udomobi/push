39f505d01a21ed60f71c9a8b85a1d51fc55862d0
John Cordeiro <john.dalton@ilhasoft.com.br>
2017-09-22 12:04:36 -0300

    Add push widget into push website

--- temba/context_processors.py
+++ temba/context_processors.py
@@ -1,5 +1,9 @@
+import settings
+
+
 def branding(request):
     """
     Stuff our branding into the context
     """
-    return dict(brand=request.branding)
+    return dict(brand=request.branding, websocket_address=settings.WEBSOCKET_ADDRESS, hostname=settings.HOSTNAME,
+                support_channel=settings.SUPPORT_CHANNEL)
