de44de69219838925a9ef6766c56a12bf2b10df7
Eduardo Douglas <edudouglas@gmail.com>
2018-09-14 13:27:32 -0300

    Send and receive email

--- temba/channels/types/email/tasks.py
+++ temba/channels/types/email/tasks.py
@@ -10,12 +10,46 @@ import re
 from celery.task import task
 from poplib import POP3_SSL
 from dateutil.parser import parse
+from functools import reduce
 
 from temba.channels.models import Channel
 from temba.contacts.models import URN
 from temba.msgs.models import Msg
 
 logger = logging.getLogger(__name__)
+
+
+class EmailBodyParse:
+    REGEX_GMAIL = r'(.*)((On|Em)(.*)(wrote|escreveu):)'
+    REGEX_OUTLOOK = r'(.*)(________________________________)'
+    REGEX_OUTLOOK_2 = r'(.*)(------------------------------)'
+    REGEX_ZOHO = r'(.*)(---- (On|Em)(.*)(wrote|escreveu) ----)'
+    SIG_REGEX = r'(.*)((--|__|-\w)|(^Sent from my (\w+\s*){1,3}))'
+
+    REGEX_LIST = [
+        REGEX_GMAIL,
+        REGEX_OUTLOOK,
+        REGEX_OUTLOOK_2,
+        REGEX_ZOHO,
+        SIG_REGEX,
+    ]
+
+    def __init__(self, text):
+        self.text = text
+
+    def get_body(self):
+        def reduce_fn(current, REGEX):
+            match = re.search(REGEX, self.text, re.DOTALL | re.MULTILINE)
+            if match:
+                result = match.group(1)
+                if len(result) < len(current):
+                    return result
+            return current
+        return reduce(reduce_fn, self.REGEX_LIST, self.text)
+
+    @property
+    def body(self):
+        return self.get_body().strip()
 
 
 @task(track_started=True, name='check_channel_mailbox')
@@ -31,26 +65,30 @@ def check_channel_mailbox():
 
                 logger.info(server.getwelcome())
                 logger.info(server.stat())
-
                 num_messages = len(server.list()[1])
 
                 for i in range(num_messages):
                     raw_email = b"\n".join(server.retr(i + 1)[1])
                     parsed_email = email.message_from_string(raw_email)
                     payload = parsed_email.get_payload()
-                    body = payload[0].get_payload()
-                    body = body.split('\n')
 
                     message_payload = StringIO.StringIO(raw_email)
                     message = rfc822.Message(message_payload)
 
-                    match = re.search(r'[\w\.-]+@[\w\.-]+', message['From'])
-                    sender = match.group(0)
+                    match_sender = re.search(r'[\w\.-]+@[\w\.-]+', message['From'])
+                    sender = match_sender.group(0)
 
+                    content = payload[0].get_payload()
+                    encoding = payload[0].get('Content-Transfer-Encoding')
+
+                    if encoding:
+                        content = content.decode(encoding)
+
+                    body = EmailBodyParse(content).get_body()
                     urn = URN.from_parts(channel.schemes[0], sender)
-                    sms = Msg.create_incoming(channel, urn, body[0], date=parse(message['Date']))
+                    sms = Msg.create_incoming(channel, urn, body, date=parse(message['Date']))
 
-                    logger.info('New Email: {}'.format(body))
+                    logger.info('New Email: {}'.format(body.decode('utf8')))
                     logger.info('SMS Accepted: {}'.format(sms.id))
                     server.dele(i + 1)
                 server.quit()
