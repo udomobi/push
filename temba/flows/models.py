0c143417771ffd099ae04e0915bfd503401c69a2
Eduardo Douglas <edudouglas@gmail.com>
2019-03-29 17:56:37 -0300

    Fix webhook

--- temba/flows/models.py
+++ temba/flows/models.py
@@ -4186,8 +4186,8 @@ class RuleSet(models.Model):
                 if RuleSet.CONFIG_WEBHOOK_HEADERS in self.config:
                     headers = self.config[RuleSet.CONFIG_WEBHOOK_HEADERS]
                     for item in headers:
-                        header[item.get('name')] = item.get('value')
-
+                        (value, errors) = Msg.evaluate_template(item.get('value'), context, org=run.flow.org)
+                        header[item.get('name')] = value
             elif self.ruleset_type == RuleSet.TYPE_RESTHOOK:
                 from temba.api.models import Resthook
 
